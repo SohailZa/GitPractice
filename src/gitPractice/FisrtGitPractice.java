f29e7e0275127fdfd9fcb1aed8a298a1977444d6
Sohail Zahid <sohailzahid@Sohails-MacBook-Pro.local>
2022-11-07 18:31:08 -0800

    this is 3 change

---
+++ src/gitPractice/FisrtGitPractice.java
@@ -0,0 +1,12 @@
+package gitPractice;
+
+public class FisrtGitPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I Love Java");
+
+	}
+
+}
