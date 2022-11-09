5895e629030f639a3928718972e7fbc722052385
Sohailcrypto <78898294+Sohailcrypto@users.noreply.github.com>
2022-11-08 17:11:55 -0800

    Update FisrtGitPractice.java
    This is from remote

--- src/gitPractice/FisrtGitPractice.java
+++ src/gitPractice/FisrtGitPractice.java
@@ -8,5 +8,10 @@ public class FisrtGitPractice {
 		System.out.println("I Love Java");
 
 	}
-
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I Love Java");
 }
+}
+
